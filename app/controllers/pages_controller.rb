37aeff9557ed2271d7ab3a4547e2c44970dbb707
Kimberly M <kcm230@nyu.edu>
2013-04-11 19:01:43 -0400

    Fix homepage logged in/logged out issue

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,8 @@
 class PagesController < ApplicationController
- 
+
  def home
-  	@trips = current_user.trips
 
-  	@trips.each do |trip| 
-		trip.description
-	end
 
-	respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @trips }
-	end 
 end
 
   def about
